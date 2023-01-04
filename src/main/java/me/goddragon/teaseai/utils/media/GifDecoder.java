518ae81e9726621936975cba70e2d86d4268e974
L. Sidelleh <leanan.sidelleh@yahoo.com>
2023-01-03 22:15:34 -0500

    Fixing more java style warnings...

--- src/main/java/me/goddragon/teaseai/utils/media/GifDecoder.java
+++ src/main/java/me/goddragon/teaseai/utils/media/GifDecoder.java
@@ -96,7 +96,7 @@ public class GifDecoder {
     protected byte[] pixelStack;
     protected byte[] pixels;
 
-    protected ArrayList frames; // frames read from current file
+    protected ArrayList<GifFrame> frames; // frames read from current file
     protected int frameCount;
 
     static class GifFrame {
@@ -119,7 +119,7 @@ public class GifDecoder {
         //
         delay = -1;
         if ((n >= 0) && (n < frameCount)) {
-            delay = ((GifFrame) frames.get(n)).delay;
+            delay = frames.get(n).delay;
         }
         return delay;
     }
@@ -251,7 +251,7 @@ public class GifDecoder {
     public BufferedImage getFrame(int n) {
         BufferedImage im = null;
         if ((n >= 0) && (n < frameCount)) {
-            im = ((GifFrame) frames.get(n)).image;
+            im = frames.get(n).image;
         }
         return im;
     }
@@ -491,7 +491,7 @@ public class GifDecoder {
     protected void init() {
         status = STATUS_OK;
         frameCount = 0;
-        frames = new ArrayList();
+        frames = new ArrayList<GifFrame>();
         gct = null;
         lct = null;
     }
